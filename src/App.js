e57fb3532e9064edabc8a6da11816f5b3fca104d
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-05 22:47:07 +0530

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-import './App.css';
 import Home from './Home';
 import Navbar from './Navbar';
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,17 @@
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>The Dojo Blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-            </div>
-        </nav>
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</a>
+      </div>
+    </nav>
      );
 }
  
 export default Navbar;
-<nav className="navbar">
-    <h1>The Dojo Blog</h1>
-    <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">Home</a>
-    </div>
-</nav>
