784bf66d20ef1eab92079f5f4185b253639e7d13
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-07 12:40:17 +0530

    programmetic redirects

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,8 +20,12 @@ const Create = () => {
         body: JSON.stringify(blog)
     }).then(()=>{
         console.log('New blog added');
-        setIsPending(false)
+        setIsPending(false);
+        // history.go(-1);
+        history.push('/');
     })
+   
+
   }
 
   return (
