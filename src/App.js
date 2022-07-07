2d298eb46a60dbd56e163f2da8bb30d06533ff58
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-07 12:51:14 +0530

    404 page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-
-import Home from './Home';
-import Navbar from './Navbar';
-import { BrowserRouter as Router,Route, Switch  } from 'react-router-dom';
-import Create from './Create';
-import BlogDetails from './BlogDetails';
+import Home from "./Home";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
-  
   return (
     <Router>
-    <div className="App">
-       <Navbar />
-      <div className="content">
-        <Switch>
-          <Route exact path="/">
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
               <Home />
-          </Route>
-          <Route path="/create">
+            </Route>
+            <Route path="/create">
               <Create />
-          </Route>
-          <Route path="/blogs/:id">
+            </Route>
+            <Route path="/blogs/:id">
               <BlogDetails />
-          </Route>
-        </Switch>
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
     </Router>
   );
 }
