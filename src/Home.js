96a4858dd65f9d163afbd82807eff2581932b2fe
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-07 10:12:04 +0530

    handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,8 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-  const [isPending,setIsPending] = useState(true);
-
-  
+  const [isPending, setIsPending] = useState(true);
+  const [error,setError] = useState(null);
 
   // const handleDelete = (id) => {
   //   const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -13,25 +12,34 @@ const Home = () => {
   // };
 
   useEffect(() => {
-   setTimeout(()=>{
-    fetch('http://localhost:8000/blogs')
-    .then(res => {
-      return res.json();
-    })
-    .then(data => {
-      setBlogs(data);
-      setIsPending(false);
-    })
-   },1000)
-  }, [])
-
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          
+          if(!res.ok){
+            throw Error('Could not fetch the data for that resource !')
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch(err=> {
+          setIsPending(false);
+          setError(err.message);
+        })
+    }, 1000);
+  }, []);
 
   return (
     <div className="home">
+      { error && <div style={{color:"red"}}>{error}</div>} <br />
       {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title={'All Blogs!'} />}
+      {blogs && <BlogList blogs={blogs} title={"All Blogs!"} />}
     </div>
-  )
+  );
 };
 
 export default Home;
