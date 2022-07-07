5d16ef9c16796d8178acd3324572a70756cc8e7d
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-07 08:31:36 +0530

    useEffect dependancies

--- src/Home.js
+++ src/Home.js
@@ -8,18 +8,22 @@ const Home = () => {
     { title: "Web dev top tips", body: "lorem ipsum", author: "mario", id: 3 },
   ]);
 
+  const [name,setName] = useState('mario');
+
   const handleDelete = (id)=> {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setblogs(newBlogs);
   }
 
   useEffect(() => {
-    console.log(blogs);
-  });
+    console.log(name);
+  },[name]);
 
   return <div className="home">
     <BlogList blogs={blogs} title={"All Blogs!"} handleDelete={handleDelete}/>
     {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title={"Mario's Blogs!"}/> */}
+    <button onClick={() => setName('janaka')}>Change name</button>
+    <p>{name}</p>
   </div>;
 };
 
