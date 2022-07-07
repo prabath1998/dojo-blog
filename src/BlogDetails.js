79a9cb14cfde518402b1bd3940915f0b7de376d2
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-07 12:45:42 +0530

    delete request

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,26 +1,39 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
+  const { id } = useParams();
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
 
-    const { id } = useParams();
-    const {data:blog,error,isPending} = useFetch('http://localhost:8000/blogs/' + id);
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
-    return ( 
-        <div className="blog-details">
-            {isPending && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-            {blog && (
-                <div>
-                    <article>
-                        <h2>{blog.title}</h2>
-                        <p>Written by {blog.author}</p> <br />
-                        <div>{blog.body}</div>
-                    </article>
-                </div>
-            )}
+  return (
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <div>
+          <article>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p> <br />
+            <div>{blog.body}</div>
+            <button onClick={handleClick}>delete</button>
+          </article>
         </div>
-     );
-}
- 
+      )}
+    </div>
+  );
+};
+
 export default BlogDetails;
