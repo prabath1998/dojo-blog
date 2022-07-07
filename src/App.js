c63838a13689a224b00f54a7d026109aaf001c9f
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-07 11:45:13 +0530

    route parameters

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import Home from './Home';
 import Navbar from './Navbar';
 import { BrowserRouter as Router,Route, Switch  } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = {name:'yoshi',age:25}
-  const link = 'http://www.google.com';
+  
   return (
     <Router>
     <div className="App">
@@ -20,6 +18,9 @@ function App() {
           </Route>
           <Route path="/create">
               <Create />
+          </Route>
+          <Route path="/blogs/:id">
+              <BlogDetails />
           </Route>
         </Switch>
       </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,13 +1,18 @@
-const BlogList = ({blogs,title}) => {
-    // const blogs = props.blogs;
-    // const tittle = props.title;
+import { Link } from "react-router-dom";
+
+const BlogList = ({ blogs, title }) => {
+  // const blogs = props.blogs;
+  // const tittle = props.title;
   return (
     <div className="blog-list">
-        <h2>{title}</h2>
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
+
           {/* <button onClick={()=> handleDelete(blog.id)}>Delete blog</button> */}
         </div>
       ))}
