dc0aa65f6d6b757257da04c901600b12c90dd5c5
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-07 09:48:37 +0530

    conditional loading

--- src/Home.js
+++ src/Home.js
@@ -3,28 +3,33 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending,setIsPending] = useState(true);
 
   
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // };
 
   useEffect(() => {
+   setTimeout(()=>{
     fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-      })
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setBlogs(data);
+      setIsPending(false);
+    })
+   },1000)
   }, [])
 
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title={'All Blogs!'} handleDelete={handleDelete} />}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title={'All Blogs!'} />}
     </div>
   )
 };
